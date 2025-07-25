1c90e12d29c3fe99dbf4617b1387464a7e493778
guzhu <guzhu@fairygui.com>
2025-07-25 13:39:33 +0800

    delete unused code

--- src/Main.ts
+++ src/Main.ts
@@ -2,17 +2,7 @@ const { regClass, property } = Laya;
 
 @regClass()
 export class Main extends Laya.Script {
-    @property({ type: "string", enumSource: Object.keys(Laya.Ease) })
-    public easeType: string;
-
     onAwake(): void {
-
-        this.owner.on(Laya.Event.MOUSE_DOWN, () => {
-
-            console.log("aaa");
-
-        })
-
     }
 
     onStart() {
